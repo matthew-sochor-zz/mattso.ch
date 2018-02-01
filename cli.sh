21d0287f63cea3cc7e020944ca6fa692d6d1d45f
Matthew Sochor <matthew.sochor@gmail.com>
2018-02-01 00:11:40 -0500

    Add pycache clearn and restart to cli.sh

--- cli.sh
+++ cli.sh
@@ -33,7 +33,7 @@ fi
 
 if [[ "$2" == "--kill" ]]; then
     echo "Killing App"
-
+    rm -rf app/__pycache__
     if [[ "$1" == "--prod" ]]; then
         sudo service nginx stop
         pgrep "gunicorn" | xargs kill
@@ -41,6 +41,46 @@ if [[ "$2" == "--kill" ]]; then
 
     if [[ "$1" == "--dev" ]]; then
         pgrep "flask" | xargs kill
+    fi
+
+fi
+
+
+if [[ "$2" == "--restart" ]]; then
+    rm -rf app/__pycache__
+    if [[ "$1" == "--prod" ]]; then
+
+        echo "Killing App"
+        sudo service nginx stop
+        pgrep "gunicorn" | xargs kill
+        echo "Starting App"
+        source activate app
+
+        mkdir -p logs/app
+        mkdir -p logs/nginx
+
+        # there has to be a better way to do this with ENV vs sudo
+        sudo service nginx start
+        sudo rm -f /etc/nginx/sites-enabled/default
+        sudo rm -f /etc/nginx/sites-enabled/default
+        sudo touch /etc/nginx/sites-available/app
+        sudo cp /home/ubuntu/mattso.ch/app/nginx/app.conf /etc/nginx/sites-available/app
+        sudo rm -f /etc/nginx/sites-enabled/app
+        sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled/app
+        sudo service nginx restart
+
+        gunicorn app:app -b 127.0.0.1 --threads=2 > logs/app/app.log 2>&1 &
+    fi
+
+    if [[ "$1" == "--dev" ]]; then
+        echo "Killing App"
+        pgrep "flask" | xargs kill
+        echo "Starting App"
+
+        export FLASK_APP=app/__init__.py
+        export FLASK_DEBUG=1
+        flask run &&
+        echo "App killed"
     fi
 
 fi
